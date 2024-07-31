fea33530bd90dfda0029866184458281a771b1e5
Alan Grainger <alan.grainger@gmail.com>
2024-07-31 10:20:43 +0200

    Update docs

--- api/index.php
+++ api/index.php
@@ -25,8 +25,10 @@ $f3->route( 'POST /v1/file/create-note', 'File->createNote' );
 // Please manually set the `uid` and `private_key` in your server env.cfg,
 // and `uid` and `apiKey` in your plugin data.json file.
 
-$f3->route( 'POST /v1/account/get-key', function () {
-	$this->f3->error( 401 );
+$f3->route( 'GET /v1/account/get-key', function () {
+	echo '<h1>Setup needed</h1>' .
+	     'Please <a href="https://github.com/alangrainger/share-note-self-hosted-backend">follow the instructions in the documentation</a> ' .
+	     'to add a uid and private_key / apiKey in both your server <code>env.cfg</code> and plugin <code>data.json</code> files.';
 	die();
 } );
 
