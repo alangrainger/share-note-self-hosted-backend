fe61b44c150bc661a5f778d7cb5960a4b60a65cb
Alan Grainger <alan.grainger@gmail.com>
2024-08-02 11:35:18 +0200

    Update schema

--- schema.sql
+++ schema.sql
@@ -1,10 +1,19 @@
--- Generation Time: Jul 18, 2024 at 09:45 AM
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: database
+-- Generation Time: Aug 02, 2024 at 09:27 AM
 -- Server version: 9.0.0
 -- PHP Version: 8.2.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+--
+-- Database: `sharenote`
+--
 
 -- --------------------------------------------------------
 
@@ -15,13 +24,16 @@ SET time_zone = "+00:00";
 CREATE TABLE `files` (
                          `id` int NOT NULL,
                          `users_id` int NOT NULL,
-                         `filename` char(32) NOT NULL,
-                         `filetype` varchar(10) NOT NULL,
+                         `filename` char(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+                         `filetype` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+                         `hash` char(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
                          `created` datetime NOT NULL,
                          `updated` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
+--
+-- Indexes for dumped tables
+--
 
 --
 -- Indexes for table `files`
@@ -32,12 +44,16 @@ ALTER TABLE `files`
   ADD KEY `users_id` (`users_id`),
   ADD KEY `created` (`created`),
   ADD KEY `filename` (`filename`),
-  ADD KEY `filetype` (`filetype`);
+  ADD KEY `filetype` (`filetype`),
+  ADD KEY `hash` (`hash`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
 --
 -- AUTO_INCREMENT for table `files`
 --
 ALTER TABLE `files`
     MODIFY `id` int NOT NULL AUTO_INCREMENT;
-
 COMMIT;
